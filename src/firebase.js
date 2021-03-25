b4e11237d3c4fc72849a370478b9d82d64790a4d
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-03-25 23:57:14 +0530

    [1.1.0] Added Hosting with Firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase"
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+	apiKey: "AIzaSyAZq9aETBz0gbosnlOwP85gEATjF14yRWQ",
+	authDomain: "pixel-drawing-71934.firebaseapp.com",
+	projectId: "pixel-drawing-71934",
+	storageBucket: "pixel-drawing-71934.appspot.com",
+	messagingSenderId: "613104668320",
+	appId: "1:613104668320:web:4c49dc7f8f68f04c5983c5",
+	measurementId: "G-80C1KMBSDP",
+}
+
+firebase.initializeApp(firebaseConfig)
